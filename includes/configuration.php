184e8c9316f87e014eb478d8d92e01c5f2886c9a
ServerJosh <joshua144@icloud.com>
2015-04-03 15:12:54 +0100

    API, CSS, JS and licenses

---
+++ includes/configuration.php
@@ -0,0 +1,31 @@
+<?php 
+
+// BitSell Configuration File
+
+$lang = "nolang";
+
+
+$setup = "no";
+
+
+$license = "nolicense";
+
+
+$api_key = "noapi";
+
+$address = "website-just-here";
+
+$paypal_address = "email-here";
+
+$transaction_fee = "transaction-fee-here";
+
+// coinpay
+
+$coinpay_private = "no-coinpay-private";
+
+$coinpay_public = "no-coinpay-public";
+
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+?>
